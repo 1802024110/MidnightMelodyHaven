81c61e6532ecf343d235a0a336ecdaa8ca65886b
a1802024110 <a1802024110@gmail.com>
2024-07-30 06:43:11 +0000

    feat: 添加获取视频背景图像功能

--- lib/widgets/media/my_video_play.dart
+++ lib/widgets/media/my_video_play.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
-
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -16,14 +16,22 @@ class MyVideoPlay extends HookConsumerWidget {
     final playerMedia = Media(
         'https://user-images.githubusercontent.com/28951144/229373695-22f88f13-d18f-4288-9bf1-c3e078d83722.mp4');
     final aspectRatio = useState<double>(16 / 9); // Default aspect ratio
+    final backgroundImage = useState<Uint8List?>(null);
 
     useEffect(() {
-      player.value?.open(playerMedia).then((_) {
+      player.value?.open(playerMedia).then((_) async {
         // 获取视频的宽高比
-        if (player.value?.state.width != null && player.value?.state.height != null) {
-          aspectRatio.value = player.value!.state.width! / player.value!.state.height!;
+        if (player.value?.state.width != null &&
+            player.value?.state.height != null) {
+          aspectRatio.value =
+              player.value!.state.width! / player.value!.state.height!;
         }
+
+        // 获取视频的第一帧作为背景图像
+        final frame = await player.value!.screenshot();
+        backgroundImage.value = frame;
       });
+
       // Clean up the player when the widget is disposed
       return () {
         player.value?.dispose();
@@ -33,17 +41,18 @@ class MyVideoPlay extends HookConsumerWidget {
     return Stack(
       children: [
         // 背景封面图片
-        Positioned.fill(
-          child: ImageFiltered(
-            imageFilter: ImageFilter.blur(sigmaX: 60, sigmaY: 60),
-            child: Image.network(
-              "https://www.dmoe.cc/random.php",
-              fit: BoxFit.cover,
-              color: Colors.black.withOpacity(0.8),
-              colorBlendMode: BlendMode.dstATop,
+        if (backgroundImage.value != null)
+          Positioned.fill(
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(sigmaX: 60, sigmaY: 60),
+              child: Image.memory(
+                backgroundImage.value!,
+                fit: BoxFit.cover,
+                color: Colors.black.withOpacity(0.8),
+                colorBlendMode: BlendMode.dstATop,
+              ),
             ),
           ),
-        ),
         // 视频播放器
         Center(
           child: AspectRatio(
